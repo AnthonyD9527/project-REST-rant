f9d17d21c5db37b1f68b9c54caf8ea1cd952da7d
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2022-11-26 11:43:34 -0800

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -3,14 +3,15 @@ const app = express()
 const router = express.Router()
 
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
+
+
+
 
 
 
 
 router.get('/', (req, res) => {
+  
     let places = [{
         name: 'H-Thai-ML',
         city: 'Seattle',
@@ -26,6 +27,39 @@ router.get('/', (req, res) => {
       }]
     res.render('places/index', { places })
 })
+
+router.post('/', (req, res) => {
+  res.send('POST /places stub ')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+
+})
+
+router.get('/new', (req, res) => {
+  res.render("places/new")
+})
+
+router.get('/:id', (req, res) => {
+  res.send('GET /places/:id stub')
+})
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+
+
+
 
 
 
