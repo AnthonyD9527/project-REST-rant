22bbc967c5717bc1747e04bdce0a28d5d8997e9a
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2022-12-04 17:37:02 -0800

    Added Delete Button for comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,39 +2,46 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-  let comments = (
-    <h3 className='inactive'>
-      No comments yet!
-    </h3>
-  )
-  let rating = (
-    <h3 className='inactive'>
-      Not Yet Rated
-    </h3>
-  )
-  if (data.place.comments.length) {
-    let sumRatings = data.place.comments.reduce((tot, c) =>{
-      return tot + c.stars
-    } , 0 )
-    let averageRating = sumRatings / data.place.comments.length
-    rating = (
-      <h3>
-        {Math.round(averageRating) } stars
+    let comments = (
+      <h3 className="inactive">
+        No comments yet!
       </h3>
     )
-    comments = data.place.comments.map(c => {
-      return (
-        <div className="border">
-          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
-          <h4>{c.content}</h4>
-          <h3>
-            <stong>- {c.author}</stong>
-          </h3>
-          <h4>Rating: {c.stars}</h4>
-        </div>
+    let rating = (
+      <h3 className="inactive">
+        Not yet rated
+      </h3>
+    )
+    if (data.place.comments.length) {
+      let sumRatings = data.place.comments.reduce((tot, c) => {
+        return tot + c.stars
+      }, 0)
+      let averageRating = Math.round(sumRatings / data.place.comments.length)
+      let stars = ''
+      for (let i = 0; i < averageRating; i++) {
+        stars += '⭐️'
+      }
+      rating = (
+        <h3>
+          {stars} stars
+        </h3>
       )
-    })
-  }
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border col-sm-4">
+            <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+            <form method="POST" action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}>
+              <input type="submit" className="btn btn-danger" value="Delete Comment" />
+            </form>
+          </div>
+        )
+      })
+    }
     return (
         <Def>
           <main>
@@ -51,9 +58,6 @@ function show (data) {
                   Rating
                 </h2>
                 {rating}
-                <h3 className="inactive">
-                  Not Rated
-                </h3>
                 <br />
                 <h2>
                   Description
@@ -74,41 +78,37 @@ function show (data) {
                   </button>
                 </form>
               </div>
-            
-
-
+            </div>
             <hr />
             <h2>Comments</h2>
-            {comments}
-            <h3 className="inactive">
-              No comments yet!
-            </h3>
-            <form method='POST' action={'/places/$data.place.id}/comment'}>
-              <div className='form-group'>
-                <label htmlFor='content'>content</label>
-                <input className='form-control' id='content' name='content' required />
-              </div>
-              <div className='form-group'>
-                <label htmlFor='author'>author</label>
-                <input className='form-control' id='author' name='author' required />
-              </div>
-              <div className="form-group">
-              <label htmlForr="stars">star rating</label>
-              <input type="number" className="form-control" id="stars" min={0.5} max={5} name="stars" step={0.5} value={5} />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="rant">Rant?</label>
-                  <input className="form-control" id="rant" name="rant" type="checkbox" value={true} />
-                    </div>
-                    <input className="btn btn-primary" type="submit" value="Add Comment"/>
-              
-            </form>
-            
-
-
-
-            
+            <div className="row">
+              {comments}
             </div>
+            <hr />
+            <h2>Got Your Own Rant or Rave?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label htmlFor="rant">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-control" />
+                </div>
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>
           </main>
         </Def>
     )
